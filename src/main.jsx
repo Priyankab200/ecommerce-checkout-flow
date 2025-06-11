6aa1f3e16fddd846802d20d6f1993b9f78cb5058
Priyanka Bachhav <125236984+Priyankab200@users.noreply.github.com>
2025-06-11 21:07:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'  // ✅ CSS फाईल import केली आहे
+import './index.css'  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
